7b1445d4c4362f597b46c6ed0b61ba72fbedbf2a
imdasom <imdasom101@gmail.com>
2019-06-23 03:09:43 +0900

    팔레트 리스트 API 작업 중

---
+++ api/index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3001;
+
+const server = http.createServer((req, res) => {
+  if(req.url === '/api/palettes') {
+    if(req.method === 'GET') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('palettes list\n');
+    }
+  } else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
