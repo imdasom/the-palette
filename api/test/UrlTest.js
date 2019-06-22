7bdf210bfacd75d8109b1197d0458f6e695fa6b6
imdasom <imdasom101@gmail.com>
2019-06-23 03:10:19 +0900

    api : 테스트 코드 준비 완료

---
+++ api/test/UrlTest.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const should = require('should');
+describe('url', function() {
+  it('should return -1 when the value is not present', function() {
+    // assert.equal([1, 2, 3].indexOf(4), -1);
+    [1, 2].should.have.length(2);
+  });
+});
